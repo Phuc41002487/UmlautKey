cdfd993de5c5dec10e604493ef8387b5d5739651
unknown <phuc41002487@gmail.com>
2026-04-03 13:26:53 +0200

    First Commit

---
+++ README.md
@@ -0,0 +1,34 @@
+Purpose
+--
+The application is developed to support typing three umlauts (ä, ö, ü) and the sharp s (ß, called Eszett or scharfes S) on a non-German layour keyboard.<br>
+Currently there is no GUI but there will be soon.
+
+How to use
+--
+To run the application simply run the UmlautKey.exe in CMD.<br>
+
+The rule is as follow:<br>
+ae -> ä<br>
+ue -> ü<br>
+oe -> ö<br>
+ss -> ß
+
+To escape the special character, just type the last character to cancel it (e or s) just like the following:<br>
+aee -> ae<br>
+uee -> ue<br>
+oee -> oe<br>
+sss -> ss
+
+FAQ
+--
+1. Does this project use AI code?<br>
+Of course. What are you thinking?
+
+2. What is the next update?<br>
+Adding more comment so I can understand chatGPT code.<br>
+
+3. Why it is in NIM?<br>
+Because I am trying to learn NIM.
+
+4. If you are trying to learn NIM, why did you use AI?<br>
+Because I am lazy.
---
+++ UmlautKey.nim
@@ -0,0 +1,115 @@
+import winim/lean
+
+# Tracks the last character typed before a replacement
+var buffer: char = '\0'
+# Tracks whether the last character was replaced (e.g., u → ü)
+var lastReplaced: bool = false
+
+proc sendBackspace() =
+  keybd_event(VK_BACK.uint8, 0, 0, 0)
+  keybd_event(VK_BACK.uint8, 0, KEYEVENTF_KEYUP, 0)
+
+proc sendUnicodeChar(ch: WCHAR) =
+  var input: INPUT
+  input.`type` = INPUT_KEYBOARD
+  input.ki.wVk = 0
+  input.ki.wScan = ch
+  input.ki.dwFlags = KEYEVENTF_UNICODE
+  SendInput(1, addr input, sizeof(INPUT).int32)
+
+  # key up
+  input.ki.dwFlags = KEYEVENTF_UNICODE or KEYEVENTF_KEYUP
+  SendInput(1, addr input, sizeof(INPUT).int32)
+
+proc hookProc(nCode: int32, wParam: WPARAM, lParam: LPARAM): LRESULT {.stdcall.} =
+  if nCode >= 0 and wParam == WM_KEYDOWN:
+    let kb = cast[PKBDLLHOOKSTRUCT](lParam)
+    let vk = kb.vkCode
+
+    # Only handle letters A-Z
+    if vk >= 0x41 and vk <= 0x5A:
+      let ch = char(vk + 32) # lowercase
+
+      # ---- Escape logic ----
+      # If the last key triggered a replacement and the user types 'e', undo it
+      if lastReplaced and buffer == 'u' and ch == 'e':
+        sendBackspace()           # remove ü
+        sendUnicodeChar('u'.ord)  # type 'u'
+        sendUnicodeChar('e'.ord)  # type 'e'
+        buffer = '\0'
+        lastReplaced = false
+        return 1
+
+      if lastReplaced and buffer == 'o' and ch == 'e':
+        sendBackspace()           # remove ö
+        sendUnicodeChar('o'.ord)
+        sendUnicodeChar('e'.ord)
+        buffer = '\0'
+        lastReplaced = false
+        return 1      
+
+      if lastReplaced and buffer == 'a' and ch == 'e':
+        sendBackspace()           # remove ä
+        sendUnicodeChar('a'.ord)
+        sendUnicodeChar('e'.ord)
+        buffer = '\0'
+        lastReplaced = false
+        return 1
+
+      if lastReplaced and buffer == 's' and ch == 's':
+        sendBackspace()           # remove ß
+        sendUnicodeChar('s'.ord)
+        sendUnicodeChar('s'.ord)
+        buffer = '\0'
+        lastReplaced = false
+        return 1
+
+      # ---- Normal replacements ----
+      if buffer == 'u' and ch == 'e':
+        sendBackspace()
+        sendUnicodeChar(0x00FC) # ü
+        lastReplaced = true
+        buffer = 'u'
+        return 1
+
+      if buffer == 'a' and ch == 'e':
+        sendBackspace()
+        sendUnicodeChar(0x00E4) # ä
+        lastReplaced = true
+        buffer = 'a'
+        return 1
+
+      if buffer == 'o' and ch == 'e':
+        sendBackspace()
+        sendUnicodeChar(0x00F6) # ö
+        lastReplaced = true
+        buffer = 'o'
+        return 1
+
+      if buffer == 's' and ch == 's':
+        sendBackspace()
+        sendUnicodeChar(0x00DF) # ß
+        lastReplaced = true
+        buffer = 's'
+        return 1
+
+      # If no replacement triggered, update buffer
+      buffer = ch
+      lastReplaced = false
+
+    else:
+      # Non-letter key resets buffer
+      buffer = '\0'
+      lastReplaced = false
+
+  return CallNextHookEx(0, nCode, wParam, lParam)
+
+# Main
+let hook = SetWindowsHookEx(WH_KEYBOARD_LL, hookProc, 0, 0)
+
+var msg: MSG
+while GetMessage(addr msg, 0, 0, 0) != 0:
+  TranslateMessage(addr msg)
+  DispatchMessage(addr msg)
+
+UnhookWindowsHookEx(hook)
